87b4b8ddd33bfab8011ad837d9f10ae7ed9adbc8
snur0000 <31137924+snur0000@users.noreply.github.com>
2020-05-01 12:51:18 -0400

    make it better

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useUnifiedTopology', true)
-var url = 'mongodb://localhost:27017/colorsDB';
+var url = 'mongodb://snur0000:@Barbie101@ds133875.mlab.com:33875/heroku_hdn28dkz'
+//var url = 'mongodb://localhost:27017/colorsDB';
 mongoose.connect
 (url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
