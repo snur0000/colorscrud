27f7ccd37b4e8df9decf6613bc5f66f0d3144240
snur0000 <31137924+snur0000@users.noreply.github.com>
2020-05-01 13:05:16 -0400

    changed db

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useUnifiedTopology', true)
-var url = 'mongodb://snur0000:@Barbie101@ds133875.mlab.com:33875/heroku_hdn28dkz'
-//var url = 'mongodb://localhost:27017/colorsDB';
+ //var url = 'mongodb://snur0000@email.cpcc.edu:@Barbie101@ds133875.mlab.com:33875/heroku_hdn28dkz'
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/heroku_hdn28dkz';
 mongoose.connect
 (url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
