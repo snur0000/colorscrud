5a5cc12c577e76830662fc4c3bd2f3ec9823629a
snur0000 <31137924+snur0000@users.noreply.github.com>
2020-05-01 02:10:33 -0400

    changed port

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(3000, () => {
+var PORT = process.env.PORT  || 3000
+app.listen(PORT, () => {
     console.log('Express server started at port : 3000');
 });
 
