2b27b49022fb5c2a8ee052a182351e83614b863d
snur0000 <31137924+snur0000@users.noreply.github.com>
2020-05-01 01:40:51 -0400

    change databae url

--- models/db.js
+++ models/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useUnifiedTopology', true)
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/heroku_9h0sbqw6';
 mongoose.connect
-('mongodb://localhost:27017/skateDB', { useNewUrlParser: true }, (err) => {
+(url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
