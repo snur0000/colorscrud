169650dd277c8495a478dfecebb2577f92f82fbd
snur0000 <31137924+snur0000@users.noreply.github.com>
2020-05-02 18:44:40 -0400

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useUnifiedTopology', true)
-//var url = 'mongodb://heroku_hdn28dkz:hnijlen7ntqp334ose358i7jhv@ds133875.mlab.com:33875/heroku_hdn28dkz';
 var url = 'mongodb://localhost:27017/colorsDB';
 
 mongoose.connect
